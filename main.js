23582a05da219f740b413b9124e4b8c4d665da55
jimmy81726 <jimmy81726@gmail.com>
2024-10-13 21:49:19 +0800

    hamburger line 初步設好

--- main.js
+++ main.js
@@ -1,4 +1,20 @@
-import './assets/scss/all.scss';
-import 'bootstrap/dist/js/bootstrap.min.js';
+import "./assets/scss/all.scss";
+import "bootstrap/dist/js/bootstrap.min.js";
 
-console.log("Hello world!");
+// 漢堡選單按鈕動畫
+const hamburgerBtn = document.querySelector(".navbar-toggler");
+const body = document.querySelector("body");
+
+function switchHamburger() {
+  const hamburgerLines = [
+    ...document.querySelectorAll(".navbar-toggler .hamburg-line"),
+  ];
+
+  hamburgerLines.forEach((item) => {
+    item.classList.toggle("active");
+  });
+
+  body.classList.toggle("overflow-y-hidden");
+}
+
+hamburgerBtn.addEventListener("click", switchHamburger);
