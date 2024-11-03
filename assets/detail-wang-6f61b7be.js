98f8d05aaf628fef3ef17ebec2c3109c52fb5a22
ching <a0970750975@gmail.com>
2024-11-03 11:52:16 +0800

    Updates

--- assets/detail-wang-6f61b7be.js
+++ assets/detail-wang-6f61b7be.js
@@ -1,4 +1,4 @@
-import{P as Te,c as ve}from"./main-6666f3cd.js";/*!
+import{P as Te,c as ve}from"./main-7ac47191.js";/*!
   * Bootstrap v5.3.3 (https://getbootstrap.com/)
   * Copyright 2011-2024 The Bootstrap Authors (https://github.com/twbs/bootstrap/graphs/contributors)
   * Licensed under MIT (https://github.com/twbs/bootstrap/blob/main/LICENSE)
